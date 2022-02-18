4a99a5a74249484d945206ed09b372112d111d62
halikali <tunchalikali@gmail.com>
2022-02-18 12:42:16 +0300

    complete input component

---
+++ src/components/InputComponent/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo, getAllTodo } from "../../store/actions";
+
+const InputComponent = () => {
+  const [todo, setTodo] = useState<string>("");
+  const dispatch = useDispatch();
+
+  const onChangeHandler = (e: any) => {
+    setTodo(e.target.value);
+  };
+
+  const handleAddTodo = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo !== "" && todo.trimStart() !== "") {
+      addTodo(todo);
+      setTodo("");
+      dispatch(getAllTodo());
+    }
+  };
+
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input
+        type="text"
+        value={todo}
+        onChange={onChangeHandler}
+        placeholder="Add New"
+        className="todo-input"
+      />
+    </form>
+  );
+};
+
+export default InputComponent;
