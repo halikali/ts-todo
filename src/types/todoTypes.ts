8731d4597a830ee4a8203c602cec0505389b8595
halikali <tunchalikali@gmail.com>
2022-02-18 12:45:17 +0300

    create todo types file

---
+++ src/types/todoTypes.ts
@@ -0,0 +1,36 @@
+import { DocumentData } from "firebase/firestore";
+import { ThunkDispatch } from "redux-thunk";
+
+export interface TodoState {
+  todos: any[];
+  error: string;
+  isLoad: boolean;
+}
+
+export interface Todo {
+  id: string;
+  todo: string;
+  completed: boolean;
+  deleted?: boolean;
+}
+
+interface Get_Todo_start {
+  type: "GET_TODO_REQUEST";
+}
+
+interface Get_Todo_Success {
+  type: "GET_TODO_SUCCESS";
+  payload: any[];
+}
+
+interface Get_Todo_Failure {
+  type: "GET_TODO_FAILURE";
+  error: string;
+}
+
+export type TodoActionType =
+  | Get_Todo_start
+  | Get_Todo_Success
+  | Get_Todo_Failure;
+
+export type TodoDispatch = DocumentData;
