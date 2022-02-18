426595fd41eeb6abaef05812744f100b75ea4e00
halikali <tunchalikali@gmail.com>
2022-02-18 12:44:45 +0300

    create todo reducer

---
+++ src/store/reducers/todoReducer.ts
@@ -0,0 +1,23 @@
+const initialState = {
+  todos: [],
+  error: "",
+  isLoad: false,
+};
+
+const todoReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case "GET_TODO_REQUEST":
+      return { ...state, isLoad: false };
+
+    case "GET_TODO_SUCCESS":
+      return { ...state, todos: action.payload, isLoad: true };
+
+    case "GET_TODO_FAILURE":
+      return { ...state, error: action.error, isLoad: false };
+
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
