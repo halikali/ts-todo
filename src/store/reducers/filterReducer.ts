168f604058daad6276e59a9327582ca593635cef
halikali <tunchalikali@gmail.com>
2022-02-18 12:44:25 +0300

    create filter reducer

---
+++ src/store/reducers/filterReducer.ts
@@ -0,0 +1,16 @@
+import { FilterAction } from "../../types/filterTypes";
+
+const initialState = {
+  filter: "all",
+};
+
+const filterReducer = (state = initialState, action: FilterAction) => {
+  switch (action.type) {
+    case "SET_FILTER":
+      return { ...state, filter: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
