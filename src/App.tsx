1db01e21e8e59daaf88cfb2bf07f01c066e094d6
halikali <tunchalikali@gmail.com>
2022-02-21 12:03:31 +0300

    app component is arrow function now

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.scss";
 import TodoAppHomePage from "./pages/TodoAppHomePage";
 
-function App() {
+const App = () => {
   return (
     <div className="app">
       <h1 className="app__title">Things To Do</h1>
       <TodoAppHomePage />
     </div>
   );
-}
+};
 
 export default App;
