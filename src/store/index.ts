d11276d940d27776d0dc98eec3d0a50b98d50b19
halikali <tunchalikali@gmail.com>
2022-02-22 17:01:19 +0300

    import count reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,14 @@ import todoReducer from "./reducers/todoReducer";
 import filterReducer from "./reducers/filterReducer";
 import inputReducer from "./reducers/inputReducer";
 import searchReducer from "./reducers/searchReducer";
+import counterReducer from "./reducers/countReducer";
 
 const rootReducer = combineReducers({
   todoReducer,
   filterReducer,
   inputReducer,
   searchReducer,
+  counterReducer,
 });
 
 export default rootReducer;
